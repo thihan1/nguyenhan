cad9d526d3a70f5404da0524926be12c550f793d
nguyenhan8c <nguyen.han.8c@gmail.com>
2023-03-05 21:51:40 +0700

    three commit

---
+++ Baithuchang0203/Slide47.java/src/slide47/java/Slide47Java.java
@@ -0,0 +1,17 @@
+package slide47.java;
+
+import java.util.Scanner;
+
+public class Slide47Java {
+    
+   public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int sum=0;
+       while(sum1<100){
+           System.out.println("nhap so nguyen:");
+           int number=sc.nextInt();
+          }
+       System.out.println("tong cac so nguyen la:" +sum);
+    }
+    
+}
---
+++ Baithuchang0203/Slide55.java/src/slide55/java/Slide55Java.java
@@ -0,0 +1,12 @@
+package slide55.java;
+
+public class Slide55Java {
+
+    public static void main(String[] args) {
+        System.out.println("cac so chia het cho 5 la:");
+     for(int i=0;i<=200;i++)
+         if(i%5==0 && (i/5)>0){
+          System.out.print(i + " , ");
+            }}
+    
+}
